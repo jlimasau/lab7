684c7a9c4e008eac82e027c3149744fb99f36b5e
Jeffrey Lima <jlimasau@ucsd.edu>
2017-02-23 10:36:25 -0800

    coo

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
+exports.viewMin = function(req, res){
+	res.render('index', projects);
+	projects["min"] = true;
+};
 exports.view = function(req, res){
   	res.render('index', projects);
   	projects["min"] = false;
 };
 
-exports.viewMin = function(req, res){
-	res.render('index', projects);
-	projects["min"] = true;
-};
